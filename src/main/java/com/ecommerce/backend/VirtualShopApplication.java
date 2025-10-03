a1d91e7f8bacde1a54fb43febd9c58a9f07d30fe
holsaf <87883786+holsaf@users.noreply.github.com>
2025-10-02 21:47:01 -0500

    Feat: Initial commit

---
+++ src/main/java/com/ecommerce/backend/VirtualShopApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualShopApplication.class, args);
+	}
+
+}
